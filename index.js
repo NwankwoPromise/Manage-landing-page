f6658a07062e020cc3fd9d1ac8cfff5887311c40
Nwankwo Promise <nwankwopromise30@gmail.com>
2023-05-09 14:50:42 +0100

    removed unnecessary code

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ console.log(iconText)
 const scrollDown = document.querySelector('.scroll-down')
 
 
-// function slide() {
-//     page.style.transform = `translateY(${translate}%)`
-// }
 
-console.log(scrollDown)
+
+
 setTimeout(function(){
     iconText.classList.remove('hidden-text')
 }, 1000)
@@ -17,11 +15,3 @@ setTimeout(function(){
     
 }, 3500)
 
-// setTimeout(function slide() {
-//     translate = -50
-//     // page.style.transform = `translateY(${translate}%)`
-//     mainPage.style.transform = `translateY(${translate}%)`
-// }, 5000)
-// setTimeout(function removePage() {
-//     page.style.dis
-// }, 8000)
